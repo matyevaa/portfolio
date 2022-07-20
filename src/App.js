9b377f9c5f790789674a5a40adce3474541912e3
matyevaa <matyevaa@oregonstate.edu>
2022-07-20 13:14:29 -0700

    main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
+/* Components */
+import Main from "./pages/main";
+import Works from "./pages/works";
+import ErrorPage from "./pages/404";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Main/>} />
+          <Route exact path="/works" element={<Works/>} />
+          <Route path="/404" element={<ErrorPage/>} />
+          <Route path="*" element={<Navigate to="/404" />}/>
+        </Routes>
+      </Router>
   );
 }
 
---
+++ src/pages/works.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Works = () => {
+
+  return (
+    <div className="main_div">
+      <div>
+        <h1 className="title">works</h1>
+      </div>
+
+    </div>
+  );
+
+};
+
+export default Works;
